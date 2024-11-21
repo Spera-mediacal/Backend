45b4cd029c0d8b35c41d2e8b89522e80bc283692
AmmarAbdelhalem <ammarabdelhalem19@gmail.com>
2024-11-21 13:58:16 +0200

    fix error handling errors and update the response messages for the Blood Donations endpoints

--- main.py
+++ main.py
@@ -25,14 +25,29 @@ def on_event():
 
 @app.get("/api/user/{user_id}", tags=["Blood Donations"])
 def get_user_info(user_id: str, session: Session = Depends(get_session)):
-    data = session.get(UserTB, user_id)
-    return data
+    statemnet = select(UserTB).where(UserTB.id == user_id)
+    user = session.exec(statemnet).first()
+    
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    return {'message': 'User is exist', 'user': user}
 
 @app.get("/api/user/donate/{user_id}", tags=["Blood Donations"])
 def get_user_donation_history(id: str, session: Session = Depends(get_session)):
+    user = select(UserTB).where(UserTB.id == id)
+    result = session.exec(user).first()
+    
+    if not result:
+        raise HTTPException(status_code=404, detail="User not found")
+    
     statement = select(DonationHistory).where(DonationHistory.user_id == id)
     donates = session.exec(statement).all()
-    return donates
+    
+    if not donates:
+        raise HTTPException(status_code=404, detail="This user doesn't donate yet")
+    
+    return {'message': 'found', 'donate_history': donates}
 
 @app.put("/api/user", tags=["Blood Donations"])
 def update_user_by_id(user: User, session: Session = Depends(get_session)):
@@ -64,9 +79,13 @@ def update_user_by_id(user: User, session: Session = Depends(get_session)):
 @app.post("/api/user", tags=["Blood Donations"])
 def create_user(user: User, session: Session = Depends(get_session)):
     new_user = UserTB(id=user.id, name=user.name, phone=user.phone, bloodType=user.bloodType, weight=user.weight, hight=user.hight, age=user.age, lastdonate="")
+    
+    if new_user:
+        raise HTTPException(status_code=406, detail="This user already exists")
+    
     session.add(new_user)
     session.commit()
-    return {'message': 'Done'}
+    return {'message': 'User created successfully', 'user': new_user}
 
 @app.post("/api/user/donate", tags=["Blood Donations"])
 def user_new_donate(donate: Donate, session: Session = Depends(get_session)):
@@ -76,11 +95,14 @@ def user_new_donate(donate: Donate, session: Session = Depends(get_session)):
         user.lastdonate = donate.date
         session.add(user)
         session.commit()
+    elif not user:
+        raise HTTPException(status_code=404, detail="This user not found")
+
     data = session.get(UserTB, donate.id)
     new_donate = DonationHistory(quantity=donate.quantity, date=donate.date, user=data)
     session.add(new_donate)
     session.commit()
-    return {'message': 'Done'}
+    return {'message': 'Donation created successfully', 'donation': new_donate}
 
 @app.delete('/api/user/{id}', tags=['Blood Donations'])
 def delete_user_by_id(id: str, session: Session = Depends(get_session)):
