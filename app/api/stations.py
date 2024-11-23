250fc71229cbc3f1480397f386ae36664f79578d
Ammar Abdelhalem <ammarabdelhalem19@gmail.com>
2024-11-23 19:45:15 +0200

    fix some logic

--- app/api/stations.py
+++ app/api/stations.py
@@ -13,9 +13,14 @@ def get_all_stations(session: Session = Depends(get_session)):
 
 @app.post("/api/station", tags=['Stations'])
 def create_new_station(station: Station, session: Session = Depends(get_session)):
-    data = session.get(Admins, station.admin_id)
+    data = session.get(Admins, station.admin_id)  # Admin data fetching
+    statement = select(Stations).where(Stations.admin_id == station.admin_id)  # Corrected line
+    result = session.exec(statement).first()
+    
     if not data:
-        raise HTTPException(status_code=406, detail="This admin not exists")
+        raise HTTPException(status_code=404, detail="This admin does not exist")
+    elif result:
+        raise HTTPException(status_code=406, detail="This admin already exists")
     
     new_station = Stations(name=station.name, phone=station.phone, location=station.location, admin=data)
     session.add(new_station)
@@ -24,15 +29,19 @@ def create_new_station(station: Station, session: Session = Depends(get_session)
     
     return {'message': 'New station created successfully', 'station': new_station}
 
+
 @app.put("/api/station", tags=["Stations"])
 def update_station_by_id(station: StationU, session: Session = Depends(get_session)):
     statement = select(Stations).where(Stations.id == station.id)
     existing_station = session.exec(statement).first()
     
+    statemnt_for_adminid = select(Stations).where(Station.admin_id == station.admin_id)
+    result = station.exec(statemnt_for_adminid).all()
+    
     if station.name:
         existing_station.name = station.name
-    if station.manager:
-        existing_station.manager = station.manager
+    if station.admin_id != 0:
+        existing_station.admin_id = station.admin_id
     if station.phone:
         existing_station.phone = station.phone
     if station.location:
@@ -52,6 +61,8 @@ def delete_station_by_id(id: int, session: Session = Depends(get_session)):
     
     if not station:
         raise HTTPException(status_code=404, detail="Station not found")
+    
+    station.admin_id = None
     
     session.delete(station)
     session.commit()
